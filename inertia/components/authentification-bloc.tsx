3f66f0bc1bcba72b687c4796901113edaa67d209
Guillaume <guillaume@livingdata.co>
2025-11-03 16:56:54 +0100

    Removes React's states, use Inertia's Form

--- inertia/components/authentification-bloc.tsx
+++ inertia/components/authentification-bloc.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { Link, router, usePage } from '@inertiajs/react'
+import { Form, Link, usePage } from '@inertiajs/react'
 import { Alert } from '@codegouvfr/react-dsfr/Alert'
 import { Button } from '@codegouvfr/react-dsfr/Button'
 import { Checkbox } from '@codegouvfr/react-dsfr/Checkbox'
@@ -11,31 +10,7 @@ export default function AuthentificationBloc() {
   const borderColor = fr.colors.decisions.border.default.grey.default
   const linkColor = fr.colors.decisions.text.actionHigh.blueFrance.default
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [rememberMe, setRememberMe] = useState(false)
-  const [emailError, setEmailError] = useState('')
-
   const { errors } = usePage().props
-
-  function handleSubmit(e: { preventDefault: () => void }) {
-    e.preventDefault()
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-
-    if (!emailRegex.test(email)) {
-      setEmailError('Veuillez saisir une adresse e-mail valide')
-      return
-    }
-
-    setEmailError('')
-
-    router.post('/login', {
-      email,
-      password,
-      remember_me: rememberMe,
-    })
-  }
 
   return (
     <div className="flex justify-center items-center w-full">
@@ -61,20 +36,14 @@ export default function AuthentificationBloc() {
             />
           )}
         </div>
-        <form onSubmit={handleSubmit} className="w-full fr-mt-2w">
+        <Form className="w-full fr-mt-2w" action="/login" method="post">
           <Input
             label="Adresse e-mail"
-            state={emailError ? 'error' : 'default'}
-            stateRelatedMessage={emailError}
             nativeInputProps={{
               name: 'email',
               placeholder: 'exemple@mon-email.fr',
-              type: 'text',
-              value: email,
-              onChange: (e) => {
-                setEmail(e.target.value)
-                setEmailError('')
-              },
+              type: 'email',
+              required: true,
             }}
           />
           <Input
@@ -82,8 +51,7 @@ export default function AuthentificationBloc() {
             nativeInputProps={{
               name: 'password',
               type: 'password',
-              value: password,
-              onChange: (e) => setPassword(e.target.value),
+              required: true,
             }}
           />
           <div className="fr-p-2w float-end">
@@ -92,23 +60,18 @@ export default function AuthentificationBloc() {
                 {
                   label: 'Se souvenir de moi',
                   nativeInputProps: {
-                    name: 'rememberMe',
-                    checked: rememberMe,
-                    onChange: (e) => setRememberMe(e.target.checked),
+                    name: 'remember_me',
+                    value: 'yes',
                   },
                 },
               ]}
               small
             />
           </div>
-          <Button
-            className="w-full justify-center"
-            type="submit"
-            disabled={email === '' || password === ''}
-          >
+          <Button className="w-full justify-center" type="submit">
             S'authentifier
           </Button>
-        </form>
+        </Form>
         <p className="fr-mt-3w">
           Des difficultés à vous connecter ?{' '}
           <Link href="#" style={{ color: linkColor }}>
