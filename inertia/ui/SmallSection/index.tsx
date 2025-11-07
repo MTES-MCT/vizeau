b4d26f228da1bf24d3bfee55af33fbf2a668ab1a
Maxine <maxine.lepennec@gmail.com>
2025-11-07 15:41:56 +0100

    Create <SmallSection /> UI component

---
+++ inertia/ui/SmallSection/index.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react'
+import { fr } from '@codegouvfr/react-dsfr'
+
+import Button from '@codegouvfr/react-dsfr/Button'
+
+export type SmallSectionProps = {
+  title: string
+  actionIcon?: string
+  actionLabel?: string
+  handleAction?: () => void
+  children?: ReactNode
+}
+
+export default function SmallSection({
+  title,
+  actionIcon,
+  handleAction,
+  actionLabel,
+  children,
+}: SmallSectionProps) {
+  return (
+    <section>
+      <div className="flex items-center justify-between">
+        <h6 className="fr-text--sm bold fr-mb-0">{title}</h6>
+        {handleAction && (
+          <Button
+            iconId={`fr-icon-${actionIcon}` as any}
+            onClick={handleAction}
+            priority="tertiary no outline"
+            size="small"
+            title={actionLabel || 'Action'}
+          />
+        )}
+      </div>
+
+      <div
+        className="fr-p-1w fr-mt-1w"
+        style={{
+          backgroundColor: fr.colors.decisions.background.alt.blueFrance.default,
+        }}
+      >
+        {children}
+      </div>
+    </section>
+  )
+}
