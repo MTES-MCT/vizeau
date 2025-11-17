6d12823bebea2eee3e7a596005a326d145ebde2d
Alex Pernot <alex.pernot@livingdata.co>
2025-11-17 10:53:43 +0100

    fix revue Guillaume

---
+++ inertia/functions/contacts.ts
@@ -0,0 +1,19 @@
+import { ContactJson } from '../../types/models.js'
+
+export function displayContactName(contact?: ContactJson) {
+  if (!contact) {
+    return 'N/A'
+  }
+
+  if (contact.firstName && contact.lastName) {
+    return `${contact.firstName} ${contact.lastName}`
+  }
+  if (contact.firstName) {
+    return contact.firstName
+  }
+  if (contact.lastName) {
+    return contact.lastName
+  }
+
+  return 'N/A'
+}
--- tests/unit/contact/contact.spec.ts
+++ tests/unit/contact/contact.spec.ts
@@ -1,6 +1,8 @@
 import { test } from '@japa/runner'
 import { ContactFactory } from '#database/factories/contact_factory'
 import testUtils from '@adonisjs/core/services/test_utils'
+import { ContactJson } from '../../../types/models.js'
+import { displayContactName } from '../../../inertia/functions/contacts.js'
 
 test.group('Contact CRUD', (group) => {
   group.each.setup(() => testUtils.db().withGlobalTransaction())
@@ -16,5 +18,61 @@ test.group('Contact CRUD', (group) => {
     await contact.load('exploitation')
 
     assert.exists(contact.exploitation)
+  })
+})
+
+test.group('Contact front-end functions', () => {
+  test('I can display a contact full name', async ({ assert }) => {
+    const contact: ContactJson = {
+      id: '1',
+      firstName: 'Jane',
+      lastName: 'Doe',
+      role: null,
+      email: null,
+      phoneNumber: null,
+    }
+
+    assert.equal(displayContactName(contact), 'Jane Doe')
+  })
+
+  test('I can display a contact first name only', async ({ assert }) => {
+    const contact: ContactJson = {
+      id: '2',
+      firstName: 'Jane',
+      lastName: null,
+      role: null,
+      email: null,
+      phoneNumber: null,
+    }
+
+    assert.equal(displayContactName(contact), 'Jane')
+  })
+  test('I can display a contact last name only', async ({ assert }) => {
+    const contact: ContactJson = {
+      id: '3',
+      firstName: null,
+      lastName: 'Doe',
+      role: null,
+      email: null,
+      phoneNumber: null,
+    }
+
+    assert.equal(displayContactName(contact), 'Doe')
+  })
+
+  test('I display N/A when contact has no name', async ({ assert }) => {
+    const contact: ContactJson = {
+      id: '4',
+      firstName: null,
+      lastName: null,
+      role: null,
+      email: null,
+      phoneNumber: null,
+    }
+    assert.equal(displayContactName(contact), 'N/A')
+  })
+
+  test('I display N/A when contact is undefined', async ({ assert }) => {
+    assert.equal(displayContactName(undefined), 'N/A')
   })
 })
