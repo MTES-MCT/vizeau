020e3e7559a5f368e1d8e1b23bda1bb534fb2501
Maxine <maxine.lepennec@gmail.com>
2025-11-17 11:53:00 +0100

    Add DeleteAlert component

---
+++ inertia/ui/DeleteAlert/delete-alert.stories.tsx
@@ -0,0 +1,32 @@
+import DeleteAlert, { DeleteAlertProps } from './index.js'
+
+const meta = {
+  title: 'UI/DeleteAlert',
+  component: DeleteAlert,
+  tags: ['autodocs'],
+  argTypes: {
+    title: { control: 'text' },
+    description: { control: 'text' },
+    onDelete: { action: 'deleted' },
+  },
+  args: {
+    title: 'Confirmer la suppression',
+    description: 'Êtes-vous sûr de vouloir supprimer cet élément ? Cette action est irréversible.',
+  } as DeleteAlertProps,
+}
+
+export default meta
+
+export const Défaut = {}
+
+export const Small = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithoutDescription = {
+  args: {
+    description: undefined,
+  },
+}
---
+++ inertia/ui/DeleteAlert/index.tsx
@@ -0,0 +1,54 @@
+import { fr } from '@codegouvfr/react-dsfr'
+import Button from '@codegouvfr/react-dsfr/Button'
+
+export type DeleteAlertProps = {
+  title: string
+  description?: string
+  size?: 'sm' | 'md'
+  onDelete: () => void
+}
+export default function DeleteAlert({
+  title,
+  description,
+  size = 'md',
+  onDelete,
+}: DeleteAlertProps) {
+  return (
+    <div
+      className="fr-p-2v flex"
+      style={{ border: `1px solid ${fr.colors.decisions.border.plain.error.default}` }}
+    >
+      <span
+        className={`fr-icon-error-warning-line fr-mr-2v ${size === 'sm' ? 'fr-icon--sm' : ''}`}
+        style={{ color: fr.colors.decisions.text.default.error.default }}
+      ></span>
+
+      <div className="flex flex-col">
+        <h6
+          className={`fr-m-0 bold ${size === 'sm' ? 'fr-text--sm' : 'fr-text'}`}
+          style={{ color: fr.colors.decisions.text.default.error.default }}
+        >
+          {title}
+        </h6>
+        <p
+          className={`${size === 'sm' ? 'fr-text--sm' : 'fr-text'}`}
+          style={{ color: fr.colors.decisions.text.default.error.default }}
+        >
+          {description}
+        </p>
+        <Button
+          onClick={onDelete}
+          priority="tertiary"
+          size={size === 'sm' ? 'small' : 'medium'}
+          iconId="fr-icon-delete-line"
+          style={{
+            color: fr.colors.decisions.text.default.error.default,
+            border: `1px solid ${fr.colors.decisions.border.plain.error.default}`,
+          }}
+        >
+          Supprimer
+        </Button>
+      </div>
+    </div>
+  )
+}
