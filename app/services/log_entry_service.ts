a17caaf6a5f5d055c990994ace418a1e816ba10e
Alex Pernot <alex.pernot@livingdata.co>
2025-11-18 16:27:21 +0100

    Ajout du modèle d'entrée de journal de bord

---
+++ app/services/log_entry_service.ts
@@ -0,0 +1,45 @@
+import LogEntry from '#models/log_entry'
+
+export class LogEntryService {
+  async createLogEntry(logData: {
+    notes: string
+    userId: string
+    exploitationId: string
+    tags?: number[]
+  }) {
+    const logEntry = await LogEntry.create(logData)
+
+    if (logData.tags && logData.tags.length > 0) {
+      await logEntry.related('tags').attach(logData.tags)
+    }
+
+    return logEntry
+  }
+
+  async getLogForExploitation(exploitationId: string, page = 1, pageSize = 10) {
+    return LogEntry.query()
+      .where('exploitationId', exploitationId)
+      .preload('author')
+      .preload('tags')
+      .orderBy('createdAt', 'desc')
+      .paginate(page, pageSize)
+  }
+
+  async updateLogEntry(id: string, logData: { notes?: string; tags?: number[] }) {
+    const logEntry = await LogEntry.findOrFail(id)
+    if (logData.notes) {
+      logEntry.notes = logData.notes
+      await logEntry.save()
+    }
+    if (logData.tags) {
+      await logEntry.related('tags').sync(logData.tags)
+    }
+    return logEntry
+  }
+
+  async deleteLogEntry(id: string) {
+    const logEntry = await LogEntry.findOrFail(id)
+    await logEntry.delete()
+    return logEntry
+  }
+}
---
+++ database/factories/log_entry_factory.ts
@@ -0,0 +1,16 @@
+import factory from '@adonisjs/lucid/factories'
+import LogEntry from '#models/log_entry'
+import { UserFactory } from '#database/factories/user_factory'
+import { ExploitationFactory } from '#database/factories/exploitation_factory'
+import { ExploitationTagFactory } from '#database/factories/exploitation_tag_factory'
+
+export const LogEntryFactory = factory
+  .define(LogEntry, async ({ faker }) => {
+    return {
+      notes: faker.lorem.paragraph(),
+    }
+  })
+  .relation('author', () => UserFactory)
+  .relation('exploitation', () => ExploitationFactory)
+  .relation('tags', () => ExploitationTagFactory)
+  .build()
