e86eabd3f1629ece98636479b5241f4d6825bc65
Maxine <maxine.lepennec@gmail.com>
2025-11-18 12:43:54 +0100

    Optimize Illustration render

--- inertia/ui/EmptyPlaceholder/index.tsx
+++ inertia/ui/EmptyPlaceholder/index.tsx
@@ -1,7 +1,24 @@
 import { fr } from '@codegouvfr/react-dsfr'
 import Button from "@codegouvfr/react-dsfr/Button"
 
+
 import './empty-placeholder.css'
+
+type IllustrationProps = {
+  pictogram?: React.ElementType;
+  illustrativeIcon?: string;
+};
+
+const Illustration = ({ pictogram, illustrativeIcon }: IllustrationProps) => {
+  if (pictogram) {
+    const PictogramComponent = pictogram;
+    return <PictogramComponent height={110} width={110} />;
+  }
+  if (illustrativeIcon) {
+    return <span className={`${illustrativeIcon} custom-size`} aria-hidden="true"></span>;
+  }
+  return null;
+};
 
 export type EmptyPlaceholderProps = {
   label: string;
@@ -13,30 +30,16 @@ export type EmptyPlaceholderProps = {
   buttonIcon?: string;
   handleClick?: () => void;
 }
-export default function EmptyPlaceholder ({label, pictogram, illustrativeIcon, hint, buttonLabel, actionAriaLabel, buttonIcon, handleClick}: EmptyPlaceholderProps) {
-  const PictogramComponent = pictogram;
-
-const Illustration = ()=> {
-  if(PictogramComponent) {
-    return <PictogramComponent height={110} width={110} />
-  }
-
-  if(illustrativeIcon) {
-    return <span className={`${illustrativeIcon} custom-size`} aria-hidden="true"></span>
-  }
-
-  return null
-}
+export default function EmptyPlaceholder({ label, pictogram, illustrativeIcon, hint, buttonLabel, actionAriaLabel, buttonIcon, handleClick }: EmptyPlaceholderProps) {
   return (
     <div
       className="flex flex-col items-center fr-p-4v"
-      style={{ backgroundColor: fr.colors.decisions.background.alt.blueFrance.default}}
+      style={{ backgroundColor: fr.colors.decisions.background.alt.blueFrance.default }}
     >
       <div>
         <div className="flex flex-col items-center">
-          <Illustration />
-
-         {label && <span className='fr-mt-2v' style={{fontWeight: 300}}>{label}</span>}
+          <Illustration pictogram={pictogram} illustrativeIcon={illustrativeIcon} />
+          {label && <span className='fr-mt-2v' style={{ fontWeight: 300 }}>{label}</span>}
         </div>
 
         {hint && <p className='fr-text--sm text-center fr-mt-2v'>{hint}</p>}
