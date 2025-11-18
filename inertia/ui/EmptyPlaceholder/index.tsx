e924935c8f5b0659e9dc12b9ad41544f488c6e7b
Maxine <maxine.lepennec@gmail.com>
2025-11-18 08:38:20 +0100

    Add EmptyPlaceholder component

---
+++ inertia/ui/EmptyPlaceholder/index.tsx
@@ -0,0 +1,57 @@
+import { fr } from '@codegouvfr/react-dsfr'
+import Button from "@codegouvfr/react-dsfr/Button"
+
+import './empty-placeholder.css'
+
+export type EmptyPlaceholderProps = {
+  label: string;
+  illustrativeIcon?: string;
+  pictogram?: React.ElementType;
+  hint?: string;
+  buttonLabel?: string;
+  actionAriaLabel?: string;
+  buttonIcon?: string;
+  handleClick?: () => void;
+}
+export default function EmptyPlaceholder ({label, pictogram, illustrativeIcon, hint, buttonLabel, actionAriaLabel, buttonIcon, handleClick}: EmptyPlaceholderProps) {
+  const PictogramComponent = pictogram;
+
+const Illustration = ()=> {
+  if(PictogramComponent) {
+    return <PictogramComponent height={110} width={110} />
+  }
+
+  if(illustrativeIcon) {
+    return <span className={`${illustrativeIcon} custom-size`} aria-hidden="true"></span>
+  }
+
+  return null
+}
+  return (
+    <div
+      className="flex flex-col items-center fr-p-4v"
+      style={{ backgroundColor: fr.colors.decisions.background.alt.blueFrance.default}}
+    >
+      <div>
+        <div className="flex flex-col items-center">
+          <Illustration />
+
+         {label && <span className='fr-mt-2v' style={{fontWeight: 100}}>{label}</span>}
+        </div>
+
+        {hint && <p className='fr-text--sm text-center fr-mt-2v'>{hint}</p>}
+      </div>
+
+      {buttonLabel && (
+        <Button
+          size='small'
+          iconId={buttonIcon as any}
+          onClick={handleClick}
+          aria-label={actionAriaLabel || buttonLabel}
+        >
+          {buttonLabel}
+        </Button>
+      )}
+    </div>
+  );
+}
