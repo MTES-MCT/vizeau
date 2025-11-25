8c531507fc1dbf1bd8857558203525ffd50cdf6e
Alex Pernot <alex.pernot@livingdata.co>
2025-11-25 11:32:27 +0100

    Suppression de la contrainte sur la forme juridique d'une exploitation

---
+++ database/migrations/1764066208938_drop_type_structure_check.ts
@@ -0,0 +1,14 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+import Exploitation from '#models/exploitation'
+
+export default class extends BaseSchema {
+  protected tableName = Exploitation.table
+
+  async up() {
+    this.raw(
+      `ALTER TABLE ${this.tableName} DROP CONSTRAINT IF EXISTS exploitations_type_structure_check;`
+    )
+  }
+
+  async down() {}
+}
