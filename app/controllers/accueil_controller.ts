7e237f26d40a72ab9bdaf022207e15b95c2d2935
Guillaume <guillaume@livingdata.co>
2025-12-03 15:53:53 +0100

    Mise à jour du contrôleur de l'accueil : - Récupération des dernières exploitations - Récupération des dernières notes - Ajout des noms d'exploitation aux notes

--- app/controllers/accueil_controller.ts
+++ app/controllers/accueil_controller.ts
@@ -1,7 +1,33 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import { inject } from '@adonisjs/core'
+import { ExploitationService } from '#services/exploitation_service'
+import { LogEntryService } from '#services/log_entry_service'
 
+@inject()
 export default class AccueilController {
-  async index({ inertia }: HttpContext) {
-    return inertia.render('accueil')
+  constructor(
+    public exploitationService: ExploitationService,
+    public logEntryService: LogEntryService
+  ) {}
+  async index({ inertia, auth }: HttpContext) {
+    const user = auth.getUserOrFail()
+    const latestExploitations = await this.exploitationService.queryLatestExploitations()
+    const latestLogEntries = await this.logEntryService.getLatestLogEntriesFromUser(user.id)
+    const latestLogEntriesWithExploitationNames = await Promise.all(
+      latestLogEntries.map(async (logEntry) => {
+        const exploitationName = await this.exploitationService.getExploitationName(
+          logEntry.exploitationId
+        )
+        return {
+          ...logEntry.toJSON(),
+          exploitationName,
+        }
+      })
+    )
+
+    return inertia.render('accueil', {
+      latestExploitations,
+      latestLogEntries: latestLogEntriesWithExploitationNames,
+    })
   }
 }
