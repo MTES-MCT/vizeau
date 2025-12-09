dad1410e7153414a3cec0290fbe921f8471584a2
Maxine <maxine.lepennec@gmail.com>
2025-12-09 08:39:54 +0100

    Add cultures pictograms

---
+++ inertia/functions/cultures-group.tsx
@@ -0,0 +1,159 @@
+import { has } from 'lodash-es'
+
+export type GroupeCulturauxItem = { label: string; code: string; picto: string; color: string }
+type GroupeCulturauxMap = { [key: string]: GroupeCulturauxItem }
+
+export const GROUPES_CULTURAUX: GroupeCulturauxMap = {
+  BTH: {
+    label: 'Blé tendre',
+    code: 'BTH',
+    picto: '/public/cultures-pictos/BTH.png',
+    color: '#FDFF8E',
+  },
+  MIG: {
+    label: 'Maïs grain et ensilage',
+    code: 'MIG',
+    picto: '/public/cultures-pictos/MIG.png',
+    color: '#00FC07',
+  },
+  ORG: {
+    label: 'Orge',
+    code: 'ORG',
+    picto: '/public/cultures-pictos/ORG.png',
+    color: '#F0FF62',
+  },
+  AUTC: {
+    label: 'Autres céréales',
+    code: 'AUTC',
+    picto: '/public/cultures-pictos/AUTC.png',
+    color: '#DAEB02',
+  },
+  COL: {
+    label: 'Colza',
+    code: 'COL',
+    picto: '/public/cultures-pictos/COL.png',
+    color: '#FFECB2',
+  },
+
+  TOU: {
+    label: 'Tournesol',
+    code: 'TOU',
+    picto: '/public/cultures-pictos/TOU.png',
+    color: '#FFFF00',
+  },
+  AUL: {
+    label: 'Autre oléagineux',
+    code: 'AUL',
+    picto: '/public/cultures-pictos/AUL.png',
+    color: '#FDC103',
+  },
+  PRO: {
+    label: 'Protéagineux',
+    code: 'PRO',
+    picto: '/public/cultures-pictos/PRO.png',
+    color: '#F0B401',
+  },
+  PLA: {
+    label: 'Plantes à fibres',
+    code: 'PLA',
+    picto: '/public/cultures-pictos/PLA.png',
+    color: '#BB9200',
+  },
+  SEM: {
+    label: 'Semences',
+    code: 'SEM',
+    picto: '/public/cultures-pictos/SEM.png',
+    color: '#624700',
+  },
+  RIZ: {
+    label: 'Riz',
+    code: 'RIZ',
+    picto: '/public/cultures-pictos/RIZ.png',
+    color: '#8FB5FE',
+  },
+  LEG: {
+    label: 'Légumineux à grains',
+    code: 'LEG',
+    picto: '/public/cultures-pictos/LEG.png',
+    color: '#FFA07D',
+  },
+  FOU: {
+    label: 'Fourrage',
+    code: 'FOU',
+    picto: '/public/cultures-pictos/FOU.png',
+    color: '#A0C75D',
+  },
+  PRA: {
+    label: 'Prairies permanentes',
+    code: 'PRA',
+    picto: '/public/cultures-pictos/PRA.png',
+    color: '#BFFF60',
+  },
+  PRT: {
+    label: 'Prairies temporaires',
+    code: 'PRT',
+    picto: '/public/cultures-pictos/PRT.png',
+    color: '#E0FFB3',
+  },
+  VER: {
+    label: 'Vergers',
+    code: 'VER',
+    picto: '/public/cultures-pictos/VER.png',
+    color: '#FE0100',
+  },
+  VIN: {
+    label: 'Vignes',
+    code: 'VIN',
+    picto: '/public/cultures-pictos/VIN.png',
+    color: '#DF00E4',
+  },
+  FRU: {
+    label: 'Fruits à coque',
+    code: 'FRU',
+    picto: '/public/cultures-pictos/FRU.png',
+    color: '#008001',
+  },
+  OLI: {
+    label: 'Oliviers',
+    code: 'OLI',
+    picto: '/public/cultures-pictos/OLI.png',
+    color: '#9FA600',
+  },
+  CUL: {
+    label: 'Autres cultures industrielles',
+    code: 'CUL',
+    picto: '/public/cultures-pictos/CUL.png',
+    color: '#008081',
+  },
+  LEGF: {
+    label: 'Légumes ou fleurs',
+    code: 'LEGF',
+    picto: '/public/cultures-pictos/LEGF.png',
+    color: '#FEA1CE',
+  },
+  CAS: {
+    label: 'Canne à sucre',
+    code: 'CAS',
+    picto: '/public/cultures-pictos/CAS.png',
+    color: '#0000FF',
+  },
+  ARB: {
+    label: 'Arboriculture',
+    code: 'ARB',
+    picto: '/public/cultures-pictos/ARB.png',
+    color: '#02B558',
+  },
+  DIV: {
+    label: 'Divers - non disponible',
+    code: 'DIV',
+    picto: '/public/cultures-pictos/DIV.png',
+    color: '#81017F',
+  },
+}
+
+export function getCulturesGroup({ code }: { code: string }): GroupeCulturauxItem {
+  if (!has(GROUPES_CULTURAUX, code)) {
+    return GROUPES_CULTURAUX.DIV
+  }
+  return GROUPES_CULTURAUX[code]
+}
---
+++ inertia/ui/cultures-pictos.stories.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import { values } from 'lodash-es'
+import { fr } from '@codegouvfr/react-dsfr'
+
+import { GROUPES_CULTURAUX } from '~/functions/cultures-group'
+
+import Input from '@codegouvfr/react-dsfr/Input'
+export default {
+  title: '🌱 Cultures pictos',
+}
+
+export const CulturesPictos = () => {
+  const groupesArray = values(GROUPES_CULTURAUX)
+  const [search, setSearch] = useState('')
+
+  const filteredGroupes = groupesArray.filter(
+    (groupe) =>
+      groupe.code.toLowerCase().includes(search.toLowerCase()) ||
+      groupe.label.toLowerCase().includes(search.toLowerCase())
+  )
+
+  return (
+    <>
+      <h3>Groupe de cultures</h3>
+
+      <Input
+        label="Rechercher par code ou libellé..."
+        nativeInputProps={{
+          value: search,
+          onChange: (e) => setSearch(e.target.value),
+        }}
+        style={{ width: '33%', minWidth: '320px', marginBottom: '50px' }}
+      />
+
+      <div className='flex flex-wrap gap-6'>
+        {filteredGroupes.map((groupe) => (
+          <div
+            key={groupe.code}
+            className="flex flex-col items-center justify-between fr-p-3v rounded-sm box-border gap-3"
+            style={{
+              border: `1px solid ${fr.colors.decisions.border.default.grey.default}`,
+            }}
+          >
+            <div
+              className="w-[54px] h-[54px] flex items-center rounded justify-center fr-p-2v"
+              style={{ backgroundColor: '#FFFFFF' }}
+            >
+              <img src={groupe.picto} alt={groupe.label} className="h-full w-full object-contain" />
+            </div>
+
+            <div className="text-start fr-mb-2">
+              <div>
+                <strong>libellé :</strong> <span>{groupe.label}</span>
+              </div>
+              <div>
+                <strong>code :</strong> <span>{groupe.code}</span>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center w-full gap-1.25">
+              <div className="h-[30px] min-w-[300px] rounded" style={{ backgroundColor: groupe.color }} />
+              <span style={{ color: '#666666' }}>{groupe.color}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
