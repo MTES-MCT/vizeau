ab6a03acce400c8815f42162a3c3886dfb13476c
Alex Pernot <alex.pernot@livingdata.co>
2026-01-16 17:29:29 +0100

    Amélioration des données de test (dev) et mise en place de données MVP exclusives à la prod

--- database/factories/contact_factory.ts
+++ database/factories/contact_factory.ts
@@ -1,9 +1,10 @@
 import factory from '@adonisjs/lucid/factories'
 import Contact from '#models/contact'
 import { ExploitationFactory } from '#database/factories/exploitation_factory'
+import { fakerFR as faker } from '@faker-js/faker'
 
 export const ContactFactory = factory
-  .define(Contact, async ({ faker }) => {
+  .define(Contact, async () => {
     return {
       firstName: faker.person.firstName(),
       lastName: faker.person.lastName(),
