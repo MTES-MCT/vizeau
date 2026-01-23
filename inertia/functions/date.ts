92e4ff4edf86e1f7c7597c7a9b94eff11bcba563
Maxine <maxine.lepennec@gmail.com>
2026-01-23 16:01:45 +0100

    Create a date utils file

---
+++ inertia/functions/date.ts
@@ -0,0 +1,8 @@
+export function formatDateFr(date: Date | string): string {
+	const d = new Date(date);
+	return d.toLocaleDateString('fr-FR', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	});
+}
