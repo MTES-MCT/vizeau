42c6f9404cde128fc6894c7b0a45920085e24c37
Maxine <maxine.lepennec@gmail.com>
2026-01-23 16:20:27 +0100

    Add LogEntryInformationCard component for displaying log entry details

---
+++ inertia/components/log-entry-id/LogEntryInformationCard.tsx
@@ -0,0 +1,19 @@
+import { formatDateFr } from '~/functions/date';
+import LabelInfo from '~/ui/LabelInfo';
+import SectionCard from '~/ui/SectionCard'
+
+export type LogEntryInformationCardProps = {
+  userName: string;
+  createdAt: string;
+}
+
+export default function LogEntryInformationCard ({userName, createdAt}: LogEntryInformationCardProps) {
+  return (
+    <SectionCard title="Informations générales" size={'small'} icon="fr-icon-info-line">
+      <div className='flex flex-col gap-4'>
+        <LabelInfo icon='fr-icon-account-line' label="Auteur" info={userName || 'Utilisateur inconnu'} />
+        <LabelInfo icon='fr-icon-calendar-line' label="Date de création" info={formatDateFr(createdAt)} />
+      </div>
+    </SectionCard>
+  );
+}
