7519b10a451c8e91150561c729cf02ab0f8f80d6
Maxine <maxine.lepennec@gmail.com>
2026-01-27 12:08:24 +0100

    Add limit prop to TagsList

--- inertia/ui/TagsList/index.tsx
+++ inertia/ui/TagsList/index.tsx
@@ -7,17 +7,21 @@ export type TagsListProps = {
     iconId?: string
   }[]
   size?: 'md' | 'sm'
+  limit?: number
 }
-export default function TagsList({ tags, size = 'md' }: TagsListProps) {
+export default function TagsList({ tags, size = 'md', limit }: TagsListProps) {
   const uniqueMetas = uniqBy(tags, 'label')
 
   return (
-    <ul className="fr-badges-group">
-      {uniqueMetas.map(({ label, iconId }) => (
-        <li key={label} className="fr-mb-0">
+    <ul className="fr-badges-group gap-2 flex flex-wrap fr-mb-0 items-center">
+      {uniqueMetas.slice(0, limit).map(({ label, iconId }) => (
+        <li key={label} className="fr-mb-0 h-fit">
           <CustomTag label={label} iconId={iconId} size={size} />
         </li>
       ))}
+      {limit && tags.length > limit && (
+        <li className="fr-mb-0 font-thin italic">+{tags.length - limit}</li>
+      )}
     </ul>
   )
 }
