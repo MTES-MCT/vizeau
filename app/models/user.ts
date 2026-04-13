f5610dfeafbb4d37142e4221d3ddc4554e897ec7
Guillaume <guillaume@livingdata.co>
2026-04-13 17:06:08 +0200

    Ajout de la colonne openconnect_id

--- app/models/user.ts
+++ app/models/user.ts
@@ -34,6 +34,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column()
   declare email: string
 
+  @column()
+  declare openconnectId: string | null
+
   @column({ serializeAs: null })
   declare password: string
 
